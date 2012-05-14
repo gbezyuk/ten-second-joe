0c927ed06360d71ff329cae75513487fac6d17d7
Grigoriy Beziuk <gbezyuk@gmail.com>
2012-05-14 14:19:50 +0400

    admin inline for limited links

--- tsj/admin.py
+++ tsj/admin.py
@@ -6,14 +6,19 @@ Module: Ten Second Joe
 Part: Django Admin integration
 """
 from django.contrib import admin
+from django.contrib.contenttypes import generic
 from .models import LimitedLink, YoutubeLink
 
 class LimitedLinkAdmin(admin.ModelAdmin):
     readonly_fields = ['usages_count']
 
+class LimitedLinkTabularInline(generic.GenericTabularInline):
+    model = LimitedLink
+    readonly_fields = ['usages_count']
+
 class YoutubeLinkAdmin(admin.ModelAdmin):
-    
     readonly_fields = ['video_cache']
+    inlines = [LimitedLinkTabularInline]
 
 admin.site.register(LimitedLink, LimitedLinkAdmin)
 admin.site.register(YoutubeLink, YoutubeLinkAdmin)
